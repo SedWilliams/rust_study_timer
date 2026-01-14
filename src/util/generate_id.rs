dfdde30b6d75643152d530bfe5e9c4cda1615c71
tre3iii <lilshed3@gmail.com>
2026-01-14 00:21:59 -0800

    add working test for generate_id(). Useful for expanding id functionality

--- src/util/generate_id.rs
+++ src/util/generate_id.rs
@@ -5,3 +5,13 @@ pub fn generate_id() -> u32 {
     rng.random_range(1000..9999)
 }
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_generate_id() {
+        let id = generate_id();
+        assert!(id >= 1000 && id < 9999);
+    }
+}
