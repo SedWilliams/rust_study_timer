6c0a28caecf921495639500f35c1dbc3b3c0d335
tre3iii <lilshed3@gmail.com>
2026-01-13 20:51:52 -0800

    use public return type 'UnitResult' in timer()

--- src/util/timer.rs
+++ src/util/timer.rs
@@ -5,33 +5,30 @@
  *      * abstract spinner
  *******************************************/
 
-use std::{time, error::Error};
+use std::time;
 
 // ../util/
 use super::io::update_time_log;
 use super::secs_to_time_log::secs_to_time_log;
-use super::types::TimeLog;
+use super::types::{TimeLog, UnitResult};
 
 use crossterm::{
-    event::{
-        self, Event, KeyCode
-    },
+    event::{self, Event, KeyCode},
     execute,
 };
 
-pub fn timer() -> Result<(), Box<dyn Error>> {
+pub fn timer() -> UnitResult {
     //println!("Debug: timer funtion start...");
     println!("\rType 'q' to stop the timer.\r");
 
     let mut elapsed_seconds: u64 = 0;
     let start_time = std::time::Instant::now();
-    
+
     //non-mut, stack-allocated array of spinner frames
     let spinner_frames: [&str; 4] = ["|", "/", "-", "\\"];
     let mut current_frame: usize = 0;
 
     loop {
-        
         //update spinner
         if current_frame >= spinner_frames.len() {
             current_frame = 0;
@@ -44,32 +41,32 @@ pub fn timer() -> Result<(), Box<dyn Error>> {
                 start_time.elapsed().as_secs(),
                 &spinner_frames[current_frame]
             ))
-        ).expect("Crossterm error, run 'cargo fetch'");
-        current_frame+=1;
+        )
+        .expect("Crossterm error, run 'cargo fetch'");
+        current_frame += 1;
 
         //check for 'q' keypress to quit
         //poll for event every 100ms
         if event::poll(time::Duration::from_millis(100))
-            .expect("Crossterm error, run 'cargo fetch'") {
-                //if event detected, read it
-                //      error? triggers a blocking read when any key is pressed, not just 'q'
-                if let Event::Key(key) = event::read()
-                    .expect("Crossterm error, run 'cargo fetch'") {
-                        //if event was 'q' keypress, break loop
-                        //    else, continue loop
-                        if key.code == KeyCode::Char('q') {
-                            println!("\n\rTimer stopped.");
-                            println!("");
-                            break;
-                        } else {
-                            continue;
-                        }
+            .expect("Crossterm error, run 'cargo fetch'")
+        {
+            //if event detected, read it
+            //      error? triggers a blocking read when any key is pressed, not just 'q'
+            if let Event::Key(key) = event::read().expect("Crossterm error, run 'cargo fetch'") {
+                //if event was 'q' keypress, break loop
+                //    else, continue loop
+                if key.code == KeyCode::Char('q') {
+                    println!("\n\rTimer stopped.");
+                    println!("");
+                    break;
+                } else {
+                    continue;
                 }
+            }
         }
-        
+
         //calculate elapsed seconds
         elapsed_seconds = start_time.elapsed().as_secs();
-
     }
 
     let formatted_time: TimeLog = secs_to_time_log(elapsed_seconds);
